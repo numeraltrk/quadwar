39b7600fcb028c9b7ce6542950dc39816f08ae7d
t-renjith <renjith1194@gmail.com>
2026-02-04 12:46:25 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/LandingPage';
 import GamePage from './components/GamePage';
 import './index.css';
 
 function App() {
   return (
-    <Router basename="/quadwar/">
+    <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/game" element={<GamePage />} />
